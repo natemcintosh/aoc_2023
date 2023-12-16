4613a61c317de49a7748775f063d4b887601588e
Nathan McIntosh <natemcintosh@gmail.com>
2023-12-16 11:22:14 -0500

    day 15 complete

--- src/aoc_2023/day15/day15.py
+++ src/aoc_2023/day15/day15.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import NamedTuple
+from collections import defaultdict
 
 from aoc_2023.utils import format_ns
 
@@ -17,6 +19,75 @@ def HASH(s: str) -> int:
         curr = curr % 256
 
     return curr
+
+
+class Lens(NamedTuple):
+    label_str: str
+    focal_length: int
+
+
+def index_of(slabel: str, box: list[Lens]) -> int | None:
+    "Find the index of the label in the box. None if not in the box"
+    for idx, lens in enumerate(box):
+        if slabel == lens.label_str:
+            return idx
+
+    return None
+
+
+def part2(steps: list[str]) -> int:
+    """
+    The sequence of letters at the start of a step tells you the label of the lens.
+    The result of the HASH algorithm on the label tells you the box
+
+    The operation is determined by the '=' or '-'
+    - if '-', go to the correct box, and remove the correct lens (if present). Then move
+      any remaining lenses as far forward in the box as they can go
+    - if '=', the number after the '=' tells you the focal length of the lens to insert
+      in the relevant box.
+        - if there is already a lens in the box with the same label, replace it with the
+          new lens.
+        - else put it at the back of the stack of lenses
+
+    Finally, calculate the focusing power of the set of boxes and lenses.
+    The focusing power of a single lens is the result of multiplying together:
+    - One plus the box number of the lens in question.
+    - The slot number of the lens within the box: 1 for the first lens, 2 for the second
+        lens, and so on.
+    - The focal length of the lens.
+    """
+    boxes: dict[int, list[Lens]] = defaultdict(list)
+
+    for stp in steps:
+        if "-" in stp:
+            # Remove the lens with this label if it exists, and move any remaining
+            # lenses forward in the box
+            slabel = stp[:-1]
+            label = HASH(slabel)
+            assert label < 256, f"Got box index of {label}"
+            box = boxes[label]
+            if (idx := index_of(slabel, box)) is not None:
+                box.pop(idx)
+
+        else:
+            slabel, sfocal = stp.split("=", maxsplit=1)
+            label = HASH(slabel)
+            focal = int(sfocal)
+            box = boxes[label]
+            assert label < 256, f"Got box index of {label}"
+            if (idx := index_of(slabel, box)) is not None:
+                # Replace that lens with the new one
+                box[idx] = Lens(label_str=slabel, focal_length=focal)
+            else:
+                # Stick it at the back of the box
+                box.append(Lens(label_str=slabel, focal_length=focal))
+
+    # Calculate the focusing power of the boxes of lenses
+    return sum(
+        (box_num + 1) * (slot_num + 1) * lens.focal_length
+        for box_num, lenses in boxes.items()
+        for slot_num, lens in enumerate(lenses)
+    )
 
 
 if __name__ == "__main__":
@@ -38,8 +109,9 @@ if __name__ == "__main__":
 
     # === Part 2 =======================================================================
     p2_start = perf_counter_ns()
+    p2 = part2(steps)
     p2_time = format_ns(perf_counter_ns() - p2_start)
-    # print(f"Part 2: {p2}")
+    print(f"Part 2: {p2}")
 
     # === Printing =====================================================================
     print(f"\n\nSetup took {parse_time}")
--- src/aoc_2023/day15/test_day15.py
+++ src/aoc_2023/day15/test_day15.py
@@ -1,4 +1,4 @@
-from aoc_2023.day15.day15 import HASH
+from aoc_2023.day15.day15 import HASH, part2
 
 import pytest
 
@@ -21,4 +21,12 @@ hash_params = [
 @pytest.mark.parametrize("step, want", hash_params)
 def test_hash(step, want):
     got = HASH(step)
+    assert want == got
+
+
+def test_part1():
+    raw_input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+    steps = [s for s in raw_input.strip().split(",")]
+    got = part2(steps)
+    want = 145
     assert want == got
