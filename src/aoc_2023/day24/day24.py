0ed617b65a9da5a43c51ff427a4835c3646dd123
Nathan McIntosh <natemcintosh@gmail.com>
2024-01-11 23:09:37 -0500

    day 24 part 1 done

--- src/aoc_2023/day24/day24.py
+++ src/aoc_2023/day24/day24.py
@@ -1,136 +1,90 @@
-import functools
 from pathlib import Path
+from typing import NamedTuple
+from itertools import combinations
 
 from aoc_2023.utils import format_ns
 
-import numpy as np
 
-np.seterr(divide="ignore")
+class Ray(NamedTuple):
+    x: int
+    y: int
+    z: int
+    vx: int
+    vy: int
+    vz: int
 
-# https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+    @staticmethod
+    def parse(line: str) -> "Ray":
+        spos, svel = line.split("@", maxsplit=1)
+        x, y, z = spos.split(",", maxsplit=2)
+        vx, vy, vz = svel.split(",", maxsplit=2)
+        return Ray(int(x), int(y), int(z), int(vx), int(vy), int(vz))
 
+    def intersects(self, other: "Ray") -> list[float] | None:
+        """
+        Where does this ray intersect with other. If it does not, return `None`
 
-def parse_line(line: str) -> tuple[tuple[int, int, int], tuple[int, int, int]]:
-    spos, svel = line.split("@", maxsplit=1)
-    pos = tuple(int(x) for x in spos.split(",", maxsplit=2))
-    vel = tuple(int(x) for x in svel.split(",", maxsplit=2))
-    return pos, vel  # type: ignore
+        Calculate slope for each line, m.
 
+        For any pair, calculate the x-crossing with
+        x = (m1*x1 - m2*x2 + y2 - y1) / (m1 - m2)
+        plug that value into
+        y = m1*(x - x1) + y1
 
-def parse(raw_input: str) -> tuple[np.ndarray, np.ndarray]:
-    """
-    input looks like many rows of
-    176253337504656, 321166281702430, 134367602892386 @ 190, 8, 338
-    Before the '@' is position, after is velocity
+        Then check that the intersection does not happen on the wrong side of the ray
+        """
+        m1 = self.vy / self.vx
+        m2 = other.vy / other.vx
 
-    Return two nx3 arrays, first for position, second for velocity
-    """
-    pos: list[tuple[int, int, int]] = []
-    vel: list[tuple[int, int, int]] = []
+        if (m1 - m2) == 0:
+            return None
 
-    for line in raw_input.splitlines():
-        p, v = parse_line(line)
-        pos.append(p)
-        vel.append(v)
+        x_cross = ((m1 * self.x - m2 * other.x) + (other.y - self.y)) / (m1 - m2)
+        y_cross = m1 * (x_cross - self.x) + self.y
 
-    return np.array(pos, order="F"), np.array(vel, order="F")
+        # If vx > 0, x_cross must be > x
+        if ((self.vx > 0) and (x_cross < self.x)) or (
+            (other.vx > 0) and (x_cross < other.x)
+        ):
+            return None
 
+        # If vy > 0, y must be > y
+        if ((self.vy > 0) and (y_cross < self.y)) or (
+            (other.vy > 0) and (y_cross < other.y)
+        ):
+            return None
 
-def calc_ray_intersections(pos: np.ndarray, vel: np.ndarray) -> np.ndarray:
-    """
-    Input arrays are nx3. For the 2D case, ignore the 3rd column.
+        # If vx < 0, x must be < x
+        if ((self.vx < 0) and (x_cross > self.x)) or (
+            (other.vx < 0) and (x_cross > other.x)
+        ):
+            return None
 
-    Calculate slope for each line, m.
+        # If vy < 0, y must be < y
+        if ((self.vy < 0) and (y_cross > self.y)) or (
+            (other.vy < 0) and (y_cross > other.y)
+        ):
+            return None
 
-    For any pair, calculate the x-crossing with
-    x = (m1*x1 - m2*x2 + y2 - y1) / (m1 - m2)
-    plug that value into
-    y = m1*(x - x1) + y1
-    """
-    # Calculte the 2D slope
-    ms = vel[:, 1] / vel[:, 0]
-
-    # Calculate components
-    mx = ms * pos[:, 0]
-
-    # Get indices of all pairs. `idx1` is an array of all the first items, and `idx2` is
-    # an array of all the second items.
-    idx1, idx2 = np.triu_indices(mx.shape[0], k=1)
-
-    # Calculate m1*x1 - m2*x2. Note 1 - 2
-    term1 = mx[idx1] - mx[idx2]
-
-    # Calculate y2 - y1. Note 2 - 1
-    term2 = pos[:, 1][idx2] - pos[:, 1][idx1]
-
-    # Calculate m1 - m2. Note 1 - 2
-    denom = ms[idx1] - ms[idx2]
-
-    # Calculate x
-    x = (term1 + term2) / denom
-
-    # Calculate y
-    y = ms[idx1] * (x - pos[:, 0][idx1]) + pos[:, 1][idx1]
-
-    xings = np.stack((x, y), axis=1)
-
-    # When vel is negative, pos must be >= intersections
-    x_vel_is_neg = vel[:, 0] < 0
-    x_vel_is_pos = vel[:, 0] > 0
-    y_vel_is_neg = vel[:, 1] < 0
-    y_vel_is_pos = vel[:, 1] > 0
-
-    # When x vel is negative, x pos must be >= x intersections
-    x_valid_neg_xings = np.logical_and(
-        (x_vel_is_neg[idx1] & (pos[:, 0][idx1] >= x)),
-        (x_vel_is_neg[idx2] & (pos[:, 0][idx2] >= x)),
-    )
-
-    # When x vel is positive, x pos must be <= x intersections
-    x_valid_pos_xings = np.logical_and(
-        (x_vel_is_pos[idx1] & (pos[:, 0][idx1] <= x)),
-        (x_vel_is_pos[idx2] & (pos[:, 0][idx2] <= x)),
-    )
-
-    # When y vel is negative, y pos must be >= y intersections
-    y_valid_neg_xings = np.logical_and(
-        (y_vel_is_neg[idx1] & (pos[:, 1][idx1] >= y)),
-        (y_vel_is_neg[idx2] & (pos[:, 1][idx2] >= y)),
-    )
-
-    # When y vel is positive, y pos must be <= y intersections
-    y_valid_pos_xings = np.logical_and(
-        (y_vel_is_pos[idx1] & (pos[:, 1][idx1] <= y)),
-        (y_vel_is_pos[idx2] & (pos[:, 1][idx2] <= y)),
-    )
-
-    valid_ray_xings = functools.reduce(
-        np.logical_or,
-        [x_valid_neg_xings, x_valid_pos_xings, y_valid_neg_xings, y_valid_pos_xings],
-    )
-
-    xings[np.logical_not(valid_ray_xings), :] = np.inf
-    return xings
+        return [x_cross, y_cross]
 
 
-def part1(pos: np.ndarray, vel: np.ndarray, minmax: tuple[int, int]) -> int:
+def part1(rays: list[Ray], minmax: tuple[int, int]) -> int:
     """
     How many crossing points in the box defined by the min and max in the x and y
     directions.
     """
     mn, mx = minmax
-    xings = calc_ray_intersections(pos, vel)
 
-    # Are crossings on the right side of the rays?
-    # For each pair on rays, if the x velocity is negative, then x-crossing must be less
-    # than x position. If the x position
-
-    return (
-        (xings[:, 0] >= mn)
-        & (xings[:, 0] <= mx)
-        & (xings[:, 1] >= mn)
-        & (xings[:, 1] <= mx)
-    ).sum()
+    return sum(
+        1
+        for r1, r2 in combinations(rays, 2)
+        if (xing := r1.intersects(r2)) is not None
+        if xing[0] >= mn
+        if xing[0] <= mx
+        if xing[1] >= mn
+        if xing[1] <= mx
+    )
 
 
 if __name__ == "__main__":
@@ -141,12 +95,12 @@ if __name__ == "__main__":
 
     input_path = Path(__file__).parent / "input.txt"
     raw_input = input_path.read_text()
-    pos, vel = parse(raw_input)
+    rays = [Ray.parse(line) for line in raw_input.splitlines()]
     parse_time = format_ns(perf_counter_ns() - parse_start)
 
     # === Part 1 =======================================================================
     p1_start = perf_counter_ns()
-    p1 = part1(pos, vel, minmax=(200000000000000, 400000000000000))
+    p1 = part1(rays, minmax=(200000000000000, 400000000000000))
     p1_time = format_ns(perf_counter_ns() - p1_start)
     print(f"Part 1: {p1}")
 
--- src/aoc_2023/day24/test_day24.py
+++ src/aoc_2023/day24/test_day24.py
@@ -1,4 +1,24 @@
-from aoc_2023.day24.day24 import parse, part1
+from aoc_2023.day24.day24 import Ray, part1
+
+import pytest
+import numpy as np
+
+
+ps = [
+    (Ray(19, 13, 20, -2, 1, -2), Ray(18, 19, 22, -1, -1, -2), [14.33333, 15.333333]),
+    (Ray(19, 13, 20, -2, 1, -2), Ray(12, 31, 28, -1, -2, -1), [6.2, 19.4]),
+    (Ray(18, 19, 22, -1, -1, -2), Ray(20, 25, 34, -2, -2, -4), None),
+]
+
+
+@pytest.mark.parametrize("r1, r2, want", ps)
+def test_ray_intersects(r1, r2, want):
+    got = r1.intersects(r2)
+    if want is None:
+        assert want == got
+        return
+
+    assert np.allclose(want, got)
 
 
 def test_part1():
@@ -7,7 +27,7 @@ def test_part1():
 20, 25, 34 @ -2, -2, -4
 12, 31, 28 @ -1, -2, -1
 20, 19, 15 @  1, -5, -3"""
-    pos, vel = parse(raw_input)
+    rays = [Ray.parse(line) for line in raw_input.splitlines()]
     want = 2
-    got = part1(pos, vel, (7, 27))
+    got = part1(rays, (7, 27))
     assert want == got
